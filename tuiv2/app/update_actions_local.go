af8b51ac03efbe0f7676a493ab3579f379470e2b
lozzo <wuyouget@gmail.com>
2026-04-12 18:58:38 +0800

    refactor: split display local actions

--- tuiv2/app/update_actions_local.go
+++ tuiv2/app/update_actions_local.go
@@ -15,6 +15,9 @@ func (m *Model) handleLocalAction(action input.SemanticAction) (bool, tea.Cmd) {
 	if handled, cmd := m.handleWorkspaceAndTabLocalAction(action); handled {
 		return true, cmd
 	}
+	if handled, cmd := m.handleDisplayAndViewportLocalAction(action); handled {
+		return true, cmd
+	}
 	switch action.Kind {
 	case input.ActionBecomeOwner:
 		if m.runtime == nil || m.workbench == nil {
@@ -78,71 +81,6 @@ func (m *Model) handleLocalAction(action input.SemanticAction) (bool, tea.Cmd) {
 			return true, nil
 		}
 		return true, m.restartPaneTerminalCmd(paneID, pane.TerminalID)
-	case input.ActionPasteBuffer:
-		if m.mode().Kind != input.ModeDisplay {
-			return false, nil
-		}
-		m.leaveCopyMode()
-		m.setMode(input.ModeState{Kind: input.ModeNormal})
-		m.render.Invalidate()
-		return true, m.pasteBufferToActiveCmd()
-	case input.ActionPasteClipboard:
-		if m.mode().Kind != input.ModeDisplay {
-			return false, nil
-		}
-		m.leaveCopyMode()
-		m.setMode(input.ModeState{Kind: input.ModeNormal})
-		m.render.Invalidate()
-		return true, m.pasteClipboardToActiveCmd()
-	case input.ActionOpenClipboardHistory:
-		if m.mode().Kind != input.ModeDisplay {
-			return false, nil
-		}
-		return true, m.openClipboardHistory()
-	case input.ActionZoomPane:
-		if m.mode().Kind == input.ModeWorkspacePicker {
-			return false, nil
-		}
-		if m.mode().Kind == input.ModeNormal {
-			m.setMode(input.ModeState{Kind: input.ModeDisplay})
-			m.render.Invalidate()
-			return true, nil
-		}
-		if m.blocksSemanticActionForTerminalSizeLock(action) {
-			return true, m.showNotice(terminalSizeLockedNotice)
-		}
-		if m.workbench != nil {
-			if tab := m.workbench.CurrentTab(); tab != nil {
-				paneID := action.PaneID
-				if paneID == "" {
-					paneID = tab.ActivePaneID
-				}
-				enteringZoom := tab.ZoomedPaneID != paneID
-				if tab.ZoomedPaneID == paneID {
-					tab.ZoomedPaneID = ""
-				} else {
-					tab.ZoomedPaneID = paneID
-				}
-				m.setMode(input.ModeState{Kind: input.ModeNormal})
-				m.render.Invalidate()
-				return true, m.syncZoomViewportCmd(paneID, enteringZoom)
-			}
-		}
-		return true, nil
-	case input.ActionScrollUp:
-		if tab := m.workbench.CurrentTab(); tab != nil {
-			tab.ScrollOffset += 1
-			m.render.Invalidate()
-		}
-		return true, m.ensureActivePaneScrollbackCmd()
-	case input.ActionScrollDown:
-		if tab := m.workbench.CurrentTab(); tab != nil {
-			if tab.ScrollOffset > 0 {
-				tab.ScrollOffset -= 1
-			}
-			m.render.Invalidate()
-		}
-		return true, m.ensureActivePaneScrollbackCmd()
 	case input.ActionCopyModeCursorLeft:
 		return true, m.moveCopyCursor(0, -1)
 	case input.ActionCopyModeCursorRight:
---
+++ tuiv2/app/update_actions_local_display.go
@@ -0,0 +1,81 @@
+package app
+
+import (
+	tea "github.com/charmbracelet/bubbletea"
+	"github.com/lozzow/termx/tuiv2/input"
+)
+
+func (m *Model) handleDisplayAndViewportLocalAction(action input.SemanticAction) (bool, tea.Cmd) {
+	if m == nil {
+		return false, nil
+	}
+	switch action.Kind {
+	case input.ActionPasteBuffer:
+		if m.mode().Kind != input.ModeDisplay {
+			return false, nil
+		}
+		m.leaveCopyMode()
+		m.setMode(input.ModeState{Kind: input.ModeNormal})
+		m.render.Invalidate()
+		return true, m.pasteBufferToActiveCmd()
+	case input.ActionPasteClipboard:
+		if m.mode().Kind != input.ModeDisplay {
+			return false, nil
+		}
+		m.leaveCopyMode()
+		m.setMode(input.ModeState{Kind: input.ModeNormal})
+		m.render.Invalidate()
+		return true, m.pasteClipboardToActiveCmd()
+	case input.ActionOpenClipboardHistory:
+		if m.mode().Kind != input.ModeDisplay {
+			return false, nil
+		}
+		return true, m.openClipboardHistory()
+	case input.ActionZoomPane:
+		if m.mode().Kind == input.ModeWorkspacePicker {
+			return false, nil
+		}
+		if m.mode().Kind == input.ModeNormal {
+			m.setMode(input.ModeState{Kind: input.ModeDisplay})
+			m.render.Invalidate()
+			return true, nil
+		}
+		if m.blocksSemanticActionForTerminalSizeLock(action) {
+			return true, m.showNotice(terminalSizeLockedNotice)
+		}
+		if m.workbench != nil {
+			if tab := m.workbench.CurrentTab(); tab != nil {
+				paneID := action.PaneID
+				if paneID == "" {
+					paneID = tab.ActivePaneID
+				}
+				enteringZoom := tab.ZoomedPaneID != paneID
+				if tab.ZoomedPaneID == paneID {
+					tab.ZoomedPaneID = ""
+				} else {
+					tab.ZoomedPaneID = paneID
+				}
+				m.setMode(input.ModeState{Kind: input.ModeNormal})
+				m.render.Invalidate()
+				return true, m.syncZoomViewportCmd(paneID, enteringZoom)
+			}
+		}
+		return true, nil
+	case input.ActionScrollUp:
+		if tab := m.workbench.CurrentTab(); tab != nil {
+			tab.ScrollOffset += 1
+			m.render.Invalidate()
+		}
+		return true, m.ensureActivePaneScrollbackCmd()
+	case input.ActionScrollDown:
+		if tab := m.workbench.CurrentTab(); tab != nil {
+			if tab.ScrollOffset > 0 {
+				tab.ScrollOffset -= 1
+			}
+			m.render.Invalidate()
+		}
+		return true, m.ensureActivePaneScrollbackCmd()
+	default:
+		return false, nil
+	}
+}
